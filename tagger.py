e197a90c7139e7ee3794da6111d6c5dc9ffce55e
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-11-21 17:52:01 -0500

    init commit; added starter code and files

---
+++ tagger.py
@@ -0,0 +1,31 @@
+# The tagger.py starter code for CSC384 A4.
+# Currently reads in the names of the training files, test file and output file,
+# and calls the tagger (which you need to implement)
+import os
+import sys
+
+
+def tag(training_list, test_file, output_file):
+    # Tag the words from the untagged input file and write them into the output file.
+    # Doesn't do much else beyond that yet.
+    print("Tagging the file.")
+    #
+    # YOUR IMPLEMENTATION GOES HERE
+    #
+
+
+if __name__ == '__main__':
+    # Run the tagger function.
+    print("Starting the tagging process.")
+
+    # Tagger expects the input call: "python3 tagger.py -d <training files> -t <test file> -o <output file>"
+    parameters = sys.argv
+    training_list = parameters[parameters.index("-d")+1:parameters.index("-t")]
+    test_file = parameters[parameters.index("-t")+1]
+    output_file = parameters[parameters.index("-o")+1]
+    # print("Training files: " + str(training_list))
+    # print("Test file: " + test_file)
+    # print("Output file: " + output_file)
+
+    # Start the training and tagging operation.
+    tag(training_list, test_file, output_file)
---
+++ validation/tagger_validate.py
@@ -0,0 +1,37 @@
+"""This file contains the validation program for CSC384 Assignment 4.
+The validation assumes that the following files are in the same directory as the tagger_validate.py file:
+  - training1.txt  --> file of tagged words used to train the HMM tagger
+  - test1.txt      --> file of untagged words to be tagged by the HMM
+  - solution1.txt  --> file with correct tags for autotest words
+This auto grader generates a file called results.txt that records the test results.
+"""
+import os
+
+if __name__ == '__main__':
+    # Invoke the shell command to train and test the HMM tagger
+    print("Training on training1.txt, running tests on test1.txt. "
+          "Output --> output1.txt")
+    os.system("python3 tagger.py -d training1.txt -t test1.txt -o output1.txt")
+
+    # Compare the contents of the HMM tagger output with the reference solution.
+    # Store the missed cases and overall stats in results.txt
+    with open("output1.txt", "r") as output_file, \
+            open("solution1.txt", "r") as solution_file, \
+            open("results.txt", "w") as results_file:
+        # Each word is on a separate line in each file.
+        output = output_file.readlines()
+        solution = solution_file.readlines()
+        total_matches = 0
+
+        # generate the report
+        for index in range(len(output)):
+            if output[index] != solution[index]:
+                results_file.write(f"Line {index + 1}: "
+                                   f"expected <{solution[index].strip()}> "
+                                   f"but got <{output[index].strip()}>\n")
+            else:
+                total_matches = total_matches + 1
+
+        # Add stats at the end of the results file.
+        results_file.write(f"Total words seen: {len(output)}.\n")
+        results_file.write(f"Total matches: {total_matches}.\n")
