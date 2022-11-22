b173dbf03b7aa56d4cf37059e88ef89b7583ff44
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-11-22 11:52:12 -0500

    Added logic to count tag->tag transitions

--- tagger.py
+++ tagger.py
@@ -1,28 +1,84 @@
-# The tagger.py starter code for CSC384 A4.
-# Currently reads in the names of the training files, test file and output file,
-# and calls the tagger (which you need to implement)
-import os
 import sys
+from typing import *
 
 
-def tag(training_list, test_file, output_file):
-    # Tag the words from the untagged input file and write them into the output file.
-    # Doesn't do much else beyond that yet.
-    print("Tagging the file.")
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
+FreqMatrix = Dict[str, 'Frequency']
+
+
+class Frequency:
+    for_str: str
+    count: int
+    frequencies: Dict[str, int]
+
+    def __init__(self, for_str: str) -> None:
+        self.for_str = for_str
+        self.count = 0
+        self.frequencies = {}
+
+    def record(self, other_str: str) -> None:
+        if other_str not in self.frequencies:
+            self.frequencies[other_str] = 0
+        self.count += 1
+        self.frequencies[other_str] += 1
+
+    def __repr__(self) -> str:
+        return f"{self.for_str}({self.count}) -> {self.frequencies.__repr__()}"
+
+
+def parse_line(line: List[str]) -> Tuple[str, str]:
+    line = line.split(":")
+    word, tag = line[0].strip(), line[1].strip()
+    return word, tag
+
+
+def count_transition(trans_freq_matrix: FreqMatrix, tag1: str, tag2: str) -> None:
+    if tag1 not in trans_freq_matrix:
+        trans_freq_matrix[tag1] = Frequency(tag1)
+    trans_freq_matrix[tag1].record(tag2)
+
+
+def train_from_file(trans_freq_matrix: FreqMatrix, training_file: str) -> None:
+
+    with open(training_file) as file:
+
+        is_start = True
+        line1 = file.readline()
+        line2 = file.readline()
+
+        while True:
+            if not (line1 and line2):
+                break
+
+            word1, tag1 = parse_line(line1)
+            word2, tag2 = parse_line(line2)
+
+            count_transition(trans_freq_matrix, tag1, tag2)
+
+            line1 = line2
+            line2 = file.readline()
+
+
+def train_from_files(training_files: List[str]) -> None:
+
+    trans_freq_matrix: FreqMatrix = {}
+
+    for training_file in training_files:
+        train_from_file(trans_freq_matrix, training_file)
+
+    for tag in trans_freq_matrix:
+        print(trans_freq_matrix[tag])
+
+
+def tag(training_files: List[str], test_file: str, output_file: str) -> None:
+    train_from_files(training_files)
 
 
 if __name__ == '__main__':
-    # Run the tagger function.
-    print("Starting the tagging process.")
-
     # Tagger expects the input call: "python3 tagger.py -d <training files> -t <test file> -o <output file>"
     parameters = sys.argv
-    training_list = parameters[parameters.index("-d")+1:parameters.index("-t")]
-    test_file = parameters[parameters.index("-t")+1]
-    output_file = parameters[parameters.index("-o")+1]
+    training_list = parameters[parameters.index("-d") + 1 : parameters.index("-t")]
+    test_file = parameters[parameters.index("-t") + 1]
+    output_file = parameters[parameters.index("-o") + 1]
     # print("Training files: " + str(training_list))
     # print("Test file: " + test_file)
     # print("Output file: " + output_file)
---
+++ test_helpers.py
@@ -0,0 +1,34 @@
+import unittest
+from tagger import *
+
+
+class TestCountTransition(unittest.TestCase):
+    def test_basic(self):
+        transitions = [
+            ("ONE", "ONE"),
+            ("ONE", "ONE"),
+            ("ONE", "TWO"),
+            ("ONE", "ONE"),
+            ("TWO", "ONE"),
+            ("TWO", "THREE"),
+            ("TWO", "FOUR-FIVE"),
+            ("FOUR-FIVE", "ONE"),
+            ("FOUR-FIVE", "ONE"),
+        ]
+
+        trans_freq_matrix: FreqMatrix = {}
+        for tag1, tag2 in transitions:
+            count_transition(trans_freq_matrix, tag1, tag2)
+
+        self.assertEqual(3, trans_freq_matrix["ONE"].frequencies["ONE"])
+        self.assertEqual(1, trans_freq_matrix["ONE"].frequencies["TWO"])
+
+        self.assertEqual(1, trans_freq_matrix["TWO"].frequencies["ONE"])
+        self.assertEqual(1, trans_freq_matrix["TWO"].frequencies["THREE"])
+        self.assertEqual(1, trans_freq_matrix["TWO"].frequencies["FOUR-FIVE"])
+
+        self.assertEqual(2, trans_freq_matrix["FOUR-FIVE"].frequencies["ONE"])
+
+
+if __name__ == "__main__":
+    unittest.main()
