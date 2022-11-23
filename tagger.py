4e7207e2de332da47b02abe9bc7c3ff96d9153ea
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-11-23 15:50:52 -0500

    Addeded skeleton for tagging test files

--- tagger.py
+++ tagger.py
@@ -22,6 +22,16 @@ class Frequency:
         return f"{self.for_str}({self.count}) -> {self.frequencies.__repr__()}"
 
 
+class Parser:
+    def parse_line(line: List[str]) -> Tuple[str, str]:
+        line = line.split(":")
+        word, tag = line[0].strip(), line[1].strip()
+        return word, tag
+
+    def is_sentence_end(word: str) -> bool:
+        return word in [".", "!", "?"]
+
+
 class Counter:
 
     def __init__(self, training_files: List[str]) -> None:
@@ -62,11 +72,11 @@ class Counter:
     def _count(
         self, line1: str, line2: Optional[str], is_start: bool) -> bool:
 
-        word1, tag1 = self._parse_line(line1)
+        word1, tag1 = Parser.parse_line(line1)
         word2, tag2 = None, None
 
         if line2:
-            word2, tag2 = self._parse_line(line2)
+            word2, tag2 = Parser.parse_line(line2)
             self._count_transition(tag1, tag2)
 
         self._count_emission(tag1, word1)
@@ -75,16 +85,11 @@ class Counter:
             self._count_initial(tag1)
             is_start = False
 
-        if self._is_sentence_end(word1):
+        if Parser.is_sentence_end(word1):
             is_start = True
             self._sentences += 1
 
         return is_start
-
-    def _parse_line(self, line: List[str]) -> Tuple[str, str]:
-        line = line.split(":")
-        word, tag = line[0].strip(), line[1].strip()
-        return word, tag
 
     def _count_transition(self, tag1: str, tag2: str) -> None:
         if tag1 not in self._transition_freq:
@@ -101,28 +106,89 @@ class Counter:
             self._initial_freq[tag] = 0
         self._initial_freq[tag] += 1
 
-    def _is_sentence_end(self, word1: str) -> bool:
-        return word1 in [".", "!", "?"]
 
-
-def tag() -> None:
+def viterbi(
+    sentence: List[str],
+    num_sentences: int,
+    initial_freq: Dict[str, int],
+    transition_freq: FreqDict,
+    emission_freq: FreqDict) -> List[str]:
     pass
 
 
-def main(training_files: List[str], test_file: str, output_file: str) -> None:
+def tagify(sentence: List[str], tags: List[str]) -> List[str]:
+    pass
+
+
+def tag(
+    test_filename: str,
+    output_filename: str,
+    num_sentences: int,
+    initial_freq: Dict[str, int],
+    transition_freq: FreqDict,
+    emission_freq: FreqDict) -> None:
+
+    input_file = open(test_filename)
+    output_file = open(output_filename, "w")
+
+    word = input_file.readline().strip()
+    curr_sentence: List[str] = []
+
+    while word:
+
+        curr_sentence.append(word)
+        next_word = input_file.readline().strip()
+
+        # TODO: Handle cases where quotes make start/end of sentence hard to ID
+        if Parser.is_sentence_end(word):
+
+            # Call viterbi
+            most_likely_tags = viterbi(
+                curr_sentence,
+                num_sentences,
+                initial_freq,
+                transition_freq,
+                emission_freq
+            )
+
+            # Write output
+            tagged_words = tagify(curr_sentence, most_likely_tags)
+            for tagged_word in tagged_words:
+                output_file.write(tagged_word + "\n")
+
+            # Rest current sentence
+            curr_sentence = []
+
+        word = next_word
+
+    input_file.close()
+    output_file.close()
+
+
+def main(training_files: List[str], test_filename: str, output_filename: str) -> None:
+    # Count
     counter = Counter(training_files)
     num_sentences, initial_freq, transition_freq, emission_freq = counter.count()
 
+    tag(
+        test_filename,
+        output_filename,
+        num_sentences,
+        initial_freq,
+        transition_freq,
+        emission_freq
+    )
+
     # TODO: For debugging; remove this shit boi
-    print(num_sentences)
-    print("Initial")
-    print(initial_freq)
-    print("Transition")
-    for key in transition_freq:
-        print("\t" + transition_freq[key].__str__())
-    print("Emission")
-    for key in emission_freq:
-        print("\t" + emission_freq[key].__str__())
+    # print(num_sentences)
+    # print("Initial")
+    # print(initial_freq)
+    # print("Transition")
+    # for key in transition_freq:
+    #     print("\t" + transition_freq[key].__str__())
+    # print("Emission")
+    # for key in emission_freq:
+    #     print("\t" + emission_freq[key].__str__())
 
 
 if __name__ == '__main__':
